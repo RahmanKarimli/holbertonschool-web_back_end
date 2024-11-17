b9c5402a8138f4739f191fbdddd1a903d259b23a
Rahman Karimli <rahman@Rahmans-MacBook-Air.local>
2024-11-17 15:52:58 +0400

    Let's make some classrooms

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,7 +1,7 @@
 import Classroom from './0-classroom.js';
 
 function initializeRooms() {
-  let rooms = [new Classroom(19), new Classroom(20), new Classroom(34)];
+  const rooms = [new Classroom(19), new Classroom(20), new Classroom(34)];
   return rooms;
 }
 
