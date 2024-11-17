7a526f12b4d540c83e849b646ac64d5f6551f5e3
Rahman Karimli <rahman@Rahmans-MacBook-Air.local>
2024-11-17 16:50:05 +0400

    Currency

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,35 +1,33 @@
-class Currency {
+/* eslint-disable */
+export default class Currency {
   constructor(code, name) {
     this.code = code;
     this.name = name;
   }
 
-  displayFullCurrency(){
+  displayFullCurrency() {
     return `${this.name} (${this.code})`
   }
 
   get name() {
     return this._name;
   }
+
   get code() {
     return this._code;
   }
 
   set code(code) {
     if (typeof code !== 'string') {
-      throw new TypeError('code must be a string');
+      throw TypeError('code must be a string');
     }
     this._code = code;
   }
+
   set name(name) {
     if (typeof name !== 'string') {
-      throw new TypeError('name must be a string');
+      throw TypeError('name must be a string');
     }
     this._name = name;
   }
 }
-
-
-
-const dollar = new Currency(12, 'Dollars');
-console.log(dollar.displayFullCurrency());
