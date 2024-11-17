64f940cdad2fd9dc4c6a44fd542cd8ae2bc6557f
Rahman Karimli <rahman@Rahmans-MacBook-Air.local>
2024-11-17 20:08:11 +0400

    Vroom

---
+++ ES6_classes/10-car.js
@@ -0,0 +1,13 @@
+import {HolbertonClass} from "./9-hoisting";
+/* eslint-disable */
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  cloneCar() {
+    return new this.constructor(this._brand, this._motor, this._color, this._motor);
+  };
+}
