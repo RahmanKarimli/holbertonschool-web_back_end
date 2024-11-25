7dd00e6646a6a9f40dff5e7d3527d3d1605de62f
Rahman Karimli <rahman@Rahmans-MacBook-Air.local>
2024-11-26 00:57:19 +0400

    Throw an error

---
+++ ES6_promise/8-try.js
@@ -0,0 +1,7 @@
+/* eslint-disable */
+export default function divideFunction(numerator, denominator) {
+  if (denominator === 0) {
+    return new Error('cannot divide by 0');
+  }
+  return numerator/denominator
+}
