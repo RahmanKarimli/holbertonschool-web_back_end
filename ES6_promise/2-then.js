8ae5b5284fdadd5a42e551e13f21069bad4b7a9f
Rahman Karimli <rahman@Rahmans-MacBook-Air.local>
2024-11-25 21:02:30 +0400

    Catch me if you can!

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,13 +1,7 @@
 /* eslint-disable */
 export default function handleResponseFromAPI(promise) {
-  const my_promise = new Promise((resolve, reject) => {
-    if (promise) {
-      resolve({body: 'success', status: 200});
-    }
-    else {
-      reject(new Error())
-    }
-  })
-
-  my_promise.finally(console.log("Got a response from the API"));
+  return promise
+    .then({status: 200, body: 'success'})
+    .catch(new Error())
+    .finally(console.log("Got a response from the API"));
 }
