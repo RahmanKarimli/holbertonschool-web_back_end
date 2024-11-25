3f1a9005f7c99bc96a53ee72413f093235aaba10
Rahman Karimli <rahman@Rahmans-MacBook-Air.local>
2024-11-25 20:43:27 +0400

    Don't make a promise...if you know you can't keep it

---
+++ ES6_promise/1-promise.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+export default function getFullResponseFromAPI(success) {
+  const promise = new Promise((resolve, reject) => {
+    if (success) {
+      resolve({status: 200, body: 'Success'})
+    } else {
+      reject("The fake API is not working currently")
+    }
+  });
+
+  return promise
+}
