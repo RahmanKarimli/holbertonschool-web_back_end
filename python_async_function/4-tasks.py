bb2671e4868b79cc77080dcf35c93a80895d60ad
Rahman Karimli <rahman@Rahmans-MacBook-Air.local>
2024-12-08 23:27:08 +0400

    fix

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,20 +1,11 @@
 #!/usr/bin/env python3
 """Asyncoroutine module"""
 import asyncio
+from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int):
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """Returns a list of delays in ascending order."""
-    tasks = []
-    for i in range(n):
-        tasks.append(task_wait_random(max_delay))
-    delays = await asyncio.gather(*tasks)
-
-    sorted_delays = []
-    while delays:
-        smallest = min(delays)
-        sorted_delays.append(smallest)
-        delays.remove(smallest)
-
-    return sorted_delays
+    delays = [task_wait_random(max_delay) for _ in range(n)]
+    return [await delay for delay in asyncio.as_completed(delays)]
