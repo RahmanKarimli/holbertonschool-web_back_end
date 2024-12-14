c500cc02eea3e7db84e2300efc3e0816cc2cc3ae
Rahman Karimli <rahman@Rahmans-MacBook-Air.local>
2024-12-14 19:47:06 +0400

    fix

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -4,6 +4,7 @@ import asyncio
 from random import uniform
 from typing import Generator
 
+
 async def async_generator() -> Generator[float, None, None]:
     """Function for asynchronous generator."""
     for i in range(10):
