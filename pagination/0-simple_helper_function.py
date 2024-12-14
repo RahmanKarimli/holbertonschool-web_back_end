fa8334375e618bd572e4acdb9642409e71d308ff
Rahman Karimli <rahman@Rahmans-MacBook-Air.local>
2024-12-14 20:45:23 +0400

    Simple helper function

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''
+Module defines simple helper function
+'''
+from typing import Tuple
+
+
+def index_range(page, page_size):
+    '''
+    Function that returns a tuple of size two
+    containing a start index and an end index
+    '''
+    return ((page * page_size) - page_size, page * page_size)
