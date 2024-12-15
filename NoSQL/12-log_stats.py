79f3c3de19729eba78fb49e7250f1004f5112234
Rahman Karimli <rahman@Rahmans-MacBook-Air.local>
2024-12-15 21:04:34 +0400

    commit

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,28 +1,21 @@
 #!/usr/bin/env python3
-"""Log stats"""
+"""Module for logging stats"""
 from pymongo import MongoClient
 
-methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
+def log_stats():
+    """Prints statistics about Nginx logs stored in MongoDB."""
+    client = MongoClient()
+    db = client.logs
+    collection = db.nginx
 
-def log_stats(mongo_collection, option=None):
-    """Log stats"""
-    items = {}
-    if option:
-        value = mongo_collection.count_documents(
-            {"method": {"$regex": option}})
-        print(f"\tmethod {option}: {value}")
-        return
-
-    result = mongo_collection.count_documents(items)
-    print(f"{result} logs")
-    print("Methods:")
+    print(f"{collection.count_documents({})} logs")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print("Methods: ")
     for method in methods:
-        log_stats(nginx_collection, method)
-    status_check = mongo_collection.count_documents({"path": "/status"})
-    print(f"{status_check} status check")
+        print(f"\tmethod: {method}: {collection.count_documents({"method": method})}")
+    print(f"{collection.count_documents({"method": "GET", "path": "/status"})} status check")
 
 
 if __name__ == "__main__":
-    nginx_collection = MongoClient('mongodb://127.0.0.1:27017').logs.nginx
-    log_stats(nginx_collection)
+    log_stats()
