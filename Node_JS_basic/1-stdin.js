5e42f51950d2f17c3a6d8d84533bbfbed955b24e
Rahman Karimli <rahman@Rahmans-MacBook-Air.local>
2025-01-23 17:03:16 +0400

    fix

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,6 +1,10 @@
 console.log("Welcome to Holberton School, what is your name?");
-process.stdin.on('data', data => {
-  process.stdout.write(`Your name is: ${data.toString()}`);
+
+process.stdin.on('data', (data) => {
+  const name = data.toString().trim(); // Trim the input to remove newline
+  console.log(`Your name is: ${name}`);
+});
+
+process.stdin.on('end', () => {
   console.log("This important software is now closing");
-  process.exit();
-})
+});
